77f154d592b99a667dd4ca97581af321d9aa008a
Matthias Bagehorn <29986457+MatthiGamer@users.noreply.github.com>
2023-12-18 13:52:00 +0100

    Added SignalR and a Hub

--- TodoServer/Program.cs
+++ TodoServer/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Http.Connections;
+
 namespace TodoApplication
 {
     public class Program
@@ -6,11 +8,40 @@ namespace TodoApplication
         {
             // Application Builder
             WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+            ConfigureServices(builder.Services);
 
             // Application
             WebApplication application = builder.Build();
+            ConfigureApplication(application);
 
             application.Run();
+        }
+
+        public static void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSignalR();
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                    builder => builder
+                    .WithOrigins("http://localhost:3000")
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowCredentials());
+            });
+        }
+
+        private static void ConfigureApplication(WebApplication application)
+        {
+            application.UseCors("CorsPolicy");
+            application.UseRouting();
+
+            application.MapHub<TaskHub>("/Tasks", options =>
+            {
+                options.Transports = HttpTransportType.WebSockets;
+                options.AllowStatefulReconnects = true;
+            });
         }
     }
 }
---
+++ TodoServer/TaskHub.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace TodoApplication
+{
+    public class TaskHub : Hub
+    {
+        public async Task SendInitialMessage(string message)
+        {
+            Console.WriteLine($"Received initial message: {message}");
+
+            // Use the Context.ConnectionId to get the unique connection ID
+            string connectionId = Context.ConnectionId;
+
+            // Send a response back to the client (if needed)
+            await Clients.Client(connectionId).SendAsync("ReceiveInitialResponse", "Hello from ASP.NET!");
+        }
+
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
