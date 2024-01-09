911eb5feb83aa071e7483df375e24b3c01ae3581
Matthias Bagehorn <29986457+MatthiGamer@users.noreply.github.com>
2024-01-09 20:17:49 +0100

    Added TaskManagement

--- TodoServer/TaskHub.cs
+++ TodoServer/TaskHub.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using TodoServer;
 
 namespace TodoApplication
 {
@@ -18,6 +19,18 @@ namespace TodoApplication
         public async Task SendMessage(string user, string message)
         {
             await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+
+        public async Task ReceiveTask(string taskName, string listName, string? dueDateString)
+        {
+            Console.WriteLine("Task received");
+
+            if (taskName == null || listName == null) return;
+
+            TodoTask task = new TodoTask(taskName, listName, dueDateString);
+
+            Console.WriteLine("Task saved.");
+            await SendMessage(Context.User!.ToString()!, "Task saved.");
         }
     }
 }
---
+++ TodoServer/TodoTask.cs
@@ -0,0 +1,26 @@
+﻿namespace TodoServer
+{
+    public class TodoTask
+    {
+        private string name;
+        private string listName;
+        private DateTime? dueDate;
+
+        public TodoTask(string taskName, string listName, string? dueDate)
+        {
+            this.name = taskName;
+            this.listName = listName;
+
+            // Parse Date
+            bool isSuccess = DateTime.TryParse(dueDate, out DateTime temp);
+            if (isSuccess)
+            {
+                this.dueDate = temp;
+                return;
+            }
+
+            this.dueDate = null;
+            Console.WriteLine("DateConversionError", this);
+        }
+    }
+}
