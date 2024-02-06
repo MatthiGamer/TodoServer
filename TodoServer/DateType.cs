94c515326431d9ea232db939e739f62b360a2035
Matthias Bagehorn <29986457+MatthiGamer@users.noreply.github.com>
2024-02-06 11:27:16 +0100

    Renamed TodoDate to Datetype

---
+++ TodoServer/DateType.cs
@@ -0,0 +1,10 @@
+﻿namespace TodoServer
+{
+    [Serializable]
+    public struct DateType
+    {
+        public int day { get; set; }
+        public int month { get; set; }
+        public int year { get; set; }
+    }
+}
--- TodoServer/TodoTask.cs
+++ TodoServer/TodoTask.cs
@@ -6,15 +6,15 @@
         public string taskID { get; }
         public string taskName { get; set; }
         public string taskList { get; set; }
-        public TodoDate? dueDate { get; set; }
+        public DateType? dueDate { get; set; }
         public bool isImportant { get; set; }
         public bool isDone { get; set; }
 
-        public TodoTask(string taskID, string taskName, string listName, TodoDate? dueDate, bool isImportant, bool isDone)
+        public TodoTask(string taskID, string taskName, string taskList, DateType? dueDate, bool isImportant, bool isDone)
         {
             this.taskID = taskID;
             this.taskName = taskName;
-            this.taskList = listName;
+            this.taskList = taskList;
             this.dueDate = dueDate;
             this.isImportant = isImportant;
             this.isDone = isDone;
