06dc2d8bb83ef1f462540ff8f92c877e4b856433
Matthias Bagehorn <29986457+MatthiGamer@users.noreply.github.com>
2024-02-11 13:13:12 +0100

    Added more test cases for TaskManager

--- TodoServerNUnitTests/TaskManagerTests.cs
+++ TodoServerNUnitTests/TaskManagerTests.cs
@@ -1,3 +1,4 @@
+using Newtonsoft.Json;
 using TodoServer;
 
 namespace TodoServerNUnitTests
@@ -8,6 +9,7 @@ namespace TodoServerNUnitTests
         public void Setup()
         {
             // Setup before every Test
+            
         }
 
         [Test]
@@ -21,6 +23,48 @@ namespace TodoServerNUnitTests
         {
             TaskManager taskManager = TaskManager.GetInstance();
             Assert.That(taskManager, Is.SameAs(TaskManager.GetInstance()));
+        }
+
+        [Test]
+        public void TaskManagerGetTasksToJSONTest()
+        {
+            SaveTestTask();
+
+            string savedTasks = TaskManager.GetInstance().GetTasks();
+            StringAssert.AreEqualIgnoringCase("[{\"taskID\":\"ID\",\"taskName\":\"Task\",\"taskList\":\"Todo\",\"dueDate\":null,\"isImportant\":false,\"isDone\":false}]", savedTasks);
+        }
+
+        [Test]
+        public void TaskManagerGetTasksFromJSONTest()
+        {
+            SaveTestTask();
+
+            string savedTasks = TaskManager.GetInstance().GetTasks();
+            TodoTask[]? tasks = JsonConvert.DeserializeObject<TodoTask[]>(savedTasks);
+            TodoTask newTask = tasks[0];
+
+            Assert.That(newTask.taskID, Is.EqualTo("ID"));
+            Assert.That(newTask.taskName, Is.EqualTo("Task"));
+            Assert.That(newTask.taskList, Is.EqualTo("Todo"));
+            Assert.That(newTask.dueDate, Is.EqualTo(null));
+            Assert.That(newTask.isImportant, Is.EqualTo(false));
+            Assert.That(newTask.isDone, Is.EqualTo(false));
+        }
+
+        private void SaveTestTask()
+        {
+            if (TaskManager.GetInstance().GetTaskById("ID") != null) return;
+
+            TodoTask task = new TodoTask(
+                "ID",
+                "Task",
+                "Todo",
+                null,
+                false,
+                false
+            );
+
+            TaskManager.GetInstance().SaveTask(task);
         }
     }
 }
