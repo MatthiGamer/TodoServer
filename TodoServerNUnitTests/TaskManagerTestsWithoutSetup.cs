f569328a0c5d752d25fea9d3f736ddf064eaf8b8
Matthias Bagehorn <29986457+MatthiGamer@users.noreply.github.com>
2024-02-18 14:26:59 +0100

    Added class for tests that don't require setup

---
+++ TodoServerNUnitTests/TaskManagerTestsWithoutSetup.cs
@@ -0,0 +1,36 @@
+﻿using TodoServer;
+
+namespace TodoServerNUnitTests
+{
+    public class TaskManagerTestsWithoutSetup
+    {
+        [Test]
+        public void TaskManagerInstanceTest()
+        {
+            Assert.IsNotNull(TaskManager.GetInstance());
+        }
+
+        [Test]
+        public void TaskManagerSingletonEqualsTest()
+        {
+            TaskManager taskManager = TaskManager.GetInstance();
+            Assert.That(taskManager, Is.SameAs(TaskManager.GetInstance()));
+        }
+
+        [Test]
+        public void TaskManagerSaveTaskTest()
+        {
+            TodoTask task = new TodoTask(
+                TestConstants.TEST_SAVE_TASK_ID,
+                TestConstants.TEST_TASK_NAME,
+                TestConstants.TEST_TASK_LIST,
+                TestConstants.GetTestDateType(),
+                TestConstants.TEST_IS_IMPORTANT,
+                TestConstants.TEST_IS_DONE
+            );
+
+            Assert.DoesNotThrow(() => TaskManager.GetInstance().SaveTask(task));
+            Assert.DoesNotThrow(() => TaskManager.GetInstance().DeleteTask(task.taskID));
+        }
+    }
+}
