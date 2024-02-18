51b951491b87057a1896bedf0f63c224a5be5745
Matthias Bagehorn <29986457+MatthiGamer@users.noreply.github.com>
2024-02-18 14:26:20 +0100

    Added class for testing constants

---
+++ TodoServerNUnitTests/TestConstants.cs
@@ -0,0 +1,25 @@
+﻿using TodoServer;
+
+namespace TodoServerNUnitTests
+{
+    internal static class TestConstants
+    {
+        // String constants
+        public const string TEST_SAVE_TASK_ID = "ID_Save";
+
+        public const string TEST_TASK_ID = "ID";
+        public const string TEST_TASK_NAME = "Task";
+        public const string TEST_TASK_LIST = "Todo";
+
+        // Date constants
+        public const int TEST_DUE_DATE_DAY = 1;
+        public const int TEST_DUE_DATE_MONTH = 1;
+        public const int TEST_DUE_DATE_YEAR = 2024;
+
+        // Boolean constants
+        public const bool TEST_IS_IMPORTANT = false;
+        public const bool TEST_IS_DONE = false;
+
+        public static DateType GetTestDateType() => new DateType() { day = TEST_DUE_DATE_DAY, month = TEST_DUE_DATE_MONTH, year = TEST_DUE_DATE_YEAR };
+    }
+}
