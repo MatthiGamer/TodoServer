52ab993135e37ccb8317020285f14c0599900616
Matthias Bagehorn <29986457+MatthiGamer@users.noreply.github.com>
2024-03-01 17:24:09 +0100

    Added ToString() override for DateType

--- TodoServer/DateType.cs
+++ TodoServer/DateType.cs
@@ -1,10 +1,18 @@
 ﻿namespace TodoServer
 {
     [Serializable]
-    public struct DateType
+    public class DateType
     {
         public int day { get; set; }
         public int month { get; set; }
         public int year { get; set; }
+
+        public override string ToString()
+        {
+            string dayString = day < 10 ? $"0{day}" : day.ToString();
+            string monthString = day < 10 ? $"0{month}" : month.ToString();
+
+            return $"{monthString}/{dayString}/{this.year}";
+        }
     }
 }
