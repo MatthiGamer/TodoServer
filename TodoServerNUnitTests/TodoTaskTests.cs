3343ee93114b16b257820dde5d4817756ae358ae
Matthias Bagehorn <29986457+MatthiGamer@users.noreply.github.com>
2024-03-03 18:58:51 +0100

    Added test for ToString() override for TodoTask

---
+++ TodoServerNUnitTests/TodoTaskTests.cs
@@ -0,0 +1,40 @@
+﻿using TodoServer;
+
+namespace TodoServerNUnitTests
+{
+    internal class TodoTaskTests
+    {
+        [Test]
+        public void TodoTaskToStringTest()
+        {
+            TodoTask task = new TodoTask(
+                TestConstants.TEST_TASK_ID,
+                TestConstants.TEST_TASK_NAME,
+                TestConstants.TEST_TASK_LIST,
+                TestConstants.GetTestDateType(),
+                TestConstants.TEST_IS_IMPORTANT,
+                TestConstants.TEST_IS_DONE
+            );
+
+            string testString = $"{{ID: {TestConstants.TEST_TASK_ID}, " +
+                                $"Name: {TestConstants.TEST_TASK_NAME}, " +
+                                $"List: {TestConstants.TEST_TASK_LIST}, " +
+                                $"Due to: {TestConstants.GetTestDateType()}, " +
+                                $"isImportant: {TestConstants.TEST_IS_IMPORTANT}, " +
+                                $"isDone: {TestConstants.TEST_IS_DONE}}}";
+
+            Assert.That(task.ToString(), Is.EqualTo(testString));
+
+            task.dueDate = null;
+
+            testString = $"{{ID: {TestConstants.TEST_TASK_ID}, " +
+                                $"Name: {TestConstants.TEST_TASK_NAME}, " +
+                                $"List: {TestConstants.TEST_TASK_LIST}, " +
+                                "Due to: End of time, " +
+                                $"isImportant: {TestConstants.TEST_IS_IMPORTANT}, " +
+                                $"isDone: {TestConstants.TEST_IS_DONE}}}";
+
+            Assert.That(task.ToString(), Is.EqualTo(testString));
+        }
+    }
+}
