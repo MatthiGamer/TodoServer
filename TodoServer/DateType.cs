229f8ea4342c49408c69c13b1e04ab984b4af250
Matthias Bagehorn <29986457+MatthiGamer@users.noreply.github.com>
2024-03-03 18:59:16 +0100

    Added method to get DateType from string

--- TodoServer/DateType.cs
+++ TodoServer/DateType.cs
@@ -14,5 +14,24 @@
 
             return $"{monthString}/{dayString}/{this.year}";
         }
+
+        public static DateType? GetDateTypeFromString(string? date)
+        {
+            if (date == null) return null;
+
+            DateType dateType = new DateType();
+            char[] chars = date.ToCharArray();
+            try
+            {
+                dateType.month = Int32.Parse($"{chars[0]}{chars[1]}");
+                dateType.day = Int32.Parse($"{chars[3]}{chars[4]}");
+                dateType.year = Int32.Parse($"{chars[6]}{chars[7]}{chars[8]}{chars[9]}");
+            }
+            catch (Exception exception)
+            {
+                Logging.LogError("DateTypeConversionError", $"Couldn't convert string to DateType.\nError: {exception}");
+            }
+            return dateType;
+        }
     }
 }
--- TodoServerNUnitTests/DateTypeTests.cs
+++ TodoServerNUnitTests/DateTypeTests.cs
@@ -21,5 +21,23 @@ namespace TodoServerNUnitTests
             testString = $"{TestConstants.TEST_DUE_DATE_MONTH_TWO_DIGITS}/{TestConstants.TEST_DUE_DATE_DAY_TWO_DIGITS}/{TestConstants.TEST_DUE_DATE_YEAR}";
             Assert.That(dateType.ToString(), Is.EqualTo(testString));
         }
+
+        [Test]
+        public void StringToDateTypeTest()
+        {
+            string testString = $"0{TestConstants.TEST_DUE_DATE_MONTH}/0{TestConstants.TEST_DUE_DATE_DAY}/{TestConstants.TEST_DUE_DATE_YEAR}";
+            DateType dateType = DateType.GetDateTypeFromString(testString);
+
+            Assert.That(dateType.day, Is.EqualTo(TestConstants.TEST_DUE_DATE_DAY));
+            Assert.That(dateType.month, Is.EqualTo(TestConstants.TEST_DUE_DATE_MONTH));
+            Assert.That(dateType.year, Is.EqualTo(TestConstants.TEST_DUE_DATE_YEAR));
+
+            testString = $"{TestConstants.TEST_DUE_DATE_MONTH_TWO_DIGITS}/{TestConstants.TEST_DUE_DATE_DAY_TWO_DIGITS}/{TestConstants.TEST_DUE_DATE_YEAR}";
+            dateType = DateType.GetDateTypeFromString(testString);
+
+            Assert.That(dateType.day, Is.EqualTo(TestConstants.TEST_DUE_DATE_DAY_TWO_DIGITS));
+            Assert.That(dateType.month, Is.EqualTo(TestConstants.TEST_DUE_DATE_MONTH_TWO_DIGITS));
+            Assert.That(dateType.year, Is.EqualTo(TestConstants.TEST_DUE_DATE_YEAR));
+        }
     }
 }
