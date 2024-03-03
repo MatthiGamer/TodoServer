f82748cd040474a582cfb77b920cb06039a11922
Matthias Bagehorn <29986457+MatthiGamer@users.noreply.github.com>
2024-03-03 18:55:38 +0100

    Added SQLite database

---
+++ TodoServer/DatabaseManager.cs
@@ -0,0 +1,86 @@
+﻿using System.Data.Common;
+using System.Data.SQLite;
+
+namespace TodoServer
+{
+    public class DatabaseManager
+    {
+        private const string DATABASE_PATH = "TaskDB.db";
+        private const string CONNECTION_STRING = $"Data Source={DATABASE_PATH}";
+
+        private const string DATABASE_READER_ERROR_NAME = "DatabaseReaderError";
+
+        public static async Task<List<TodoTask>> GetTasksFromDB()
+        {
+            List<TodoTask> tasks = new List<TodoTask>();
+
+            TodoTask task;
+            string taskID = string.Empty;
+            string taskName = string.Empty;
+            string listName = string.Empty;
+            string? dueDateString = string.Empty;
+            DateType? dueDate = null;
+            bool isImportant = false;
+            bool isDone = false;
+
+            await using SQLiteConnection connection = new SQLiteConnection(CONNECTION_STRING);
+            await connection.OpenAsync();
+            await using SQLiteCommand command = new SQLiteCommand("SELECT * FROM Tasks", connection);
+            try
+            {
+                await using DbDataReader dataReader = await command.ExecuteReaderAsync();
+            
+
+                while (await dataReader.ReadAsync())
+                {
+                    try
+                    {
+                        taskID = dataReader.GetString(0);
+                        taskName = dataReader.GetString(1);
+                        listName = dataReader.GetString(2);
+
+                        dueDateString = dataReader.IsDBNull(3) ? null : dataReader.GetString(3);
+
+                        isImportant = IntToBool(dataReader.GetInt32(4));
+                        isDone = IntToBool(dataReader.GetInt32(5));
+
+                        dueDate = DateType.GetDateTypeFromString(dueDateString);
+
+                        task = new TodoTask(taskID, taskName, listName, dueDate, isImportant, isDone);
+                        tasks.Add(task);
+                    }
+                    catch (Exception exception)
+                    {
+                        Logging.LogError(DATABASE_READER_ERROR_NAME, $"Error converting the values from the database.\nError: {exception.Message}");
+                        continue;
+                    }
+                }
+            }
+            catch (Exception exception)
+            {
+                Logging.LogError(DATABASE_READER_ERROR_NAME, $"Error reading the database.\nError: {exception.Message}");
+            }
+
+            await connection.CloseAsync();
+            return tasks;
+        }
+
+        public static async void SaveTask(TodoTask task)
+        {
+            string valueString = $"'{task.taskID}', '{task.taskName}', '{task.listName}', {(task.dueDate != null ? $"'{task.dueDate}'" : "NULL")}, {BoolToInt(task.isImportant)}, {BoolToInt(task.isDone)}";
+            string queryString = $"INSERT INTO Tasks (ID, Name, List, DueDate, IsImportant, IsDone) VALUES ({valueString})";
+
+            await using SQLiteConnection connection = new SQLiteConnection(CONNECTION_STRING);
+            await connection.OpenAsync();
+            await using SQLiteCommand command = new SQLiteCommand(queryString, connection);
+
+            int affectedRows = await command.ExecuteNonQueryAsync();
+            if (affectedRows != 1) Logging.LogWarning("DatabaseWarning", $"Saving affected {affectedRows} rows instead of one.");
+
+            await connection.CloseAsync();
+        }
+
+        private static int BoolToInt(bool value) => value ? 1 : 0;
+        private static bool IntToBool(int value) => value == 1;
+    }
+}
